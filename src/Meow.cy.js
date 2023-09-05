56c91b3f184cc1aee3b0a7390a4253ffa72a5cce
Guillaume Chau <guillaume.b.chau@gmail.com>
2023-09-05 15:06:52 +0200

    feat: upgrade cypress + more tests

--- src/Meow.cy.js
+++ src/Meow.cy.js
@@ -1,24 +1,63 @@
 import Meow from './Meow.vue'
 
 describe('Meow.vue', () => {
+  let meows = []
+
   beforeEach(() => {
     cy.mockGraphQL({
       resolvers: {
         Query: {
-          meows: () => [
-            { id: '1', text: 'Meow!' },
-            { id: '2', text: 'Meow meow!' },
-            { id: '3', text: 'Meow meow meow!' },
-          ]
-        }
-      }
+          meows: () => meows,
+        },
+      },
     })
   })
 
-  Cypress._.times(100, () => {
-    it('loads meows', () => {
-      cy.mount(Meow)
-      cy.get('li').should('have.length', 3)
-    })
+  it('loads meows', () => {
+    meows = [
+      { id: '1', text: 'Meow!' },
+      { id: '2', text: 'Meow meow!' },
+      { id: '3', text: 'Meow meow meow!' },
+    ]
+    cy.mount(Meow)
+    cy.get('li').should('have.length', 3)
+  })
+
+  it('no meows', () => {
+    meows = []
+    cy.mount(Meow)
+    cy.get('li').should('have.length', 0)
+  })
+
+  it('one meow', () => {
+    meows = [
+      { id: '1', text: 'Meow!' },
+    ]
+    cy.mount(Meow)
+    cy.get('li').should('have.length', 1)
+  })
+
+  it('one more meow', () => {
+    meows = [
+      { id: '2', text: 'Meow meow!' },
+    ]
+    cy.mount(Meow)
+    cy.get('li').should('have.length', 1)
+  })
+
+  it('no meows again', () => {
+    meows = []
+    cy.mount(Meow)
+    cy.get('li').should('have.length', 0)
+  })
+
+  it('loads meows again?', () => {
+    meows = [
+      { id: '1', text: 'Meow!' },
+      { id: '2', text: 'Meow meow!' },
+      { id: '3', text: 'Meow meow meow!' },
+    ]
+    cy.mount(Meow)
+    cy.get('li').should('have.length', 3)
   })
 })
